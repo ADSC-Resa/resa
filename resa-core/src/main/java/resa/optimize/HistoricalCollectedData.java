cdd5b1d35348e1010d31bc109ab481e722ddd826
fuzhengjia <fuzhengjia@gmail.com>
2015-12-30 18:30:18 +0800

    release 0.1, combine current Master with Resa, new queue related metrics, 5

---
+++ resa-core/src/main/java/resa/optimize/HistoricalCollectedData.java
@@ -0,0 +1,37 @@
+package resa.optimize;
+
+import backtype.storm.generated.StormTopology;
+import resa.util.FixedSizeQueue;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Queue;
+
+/**
+ * Created by ding on 14-4-30.
+ */
+class HistoricalCollectedData {
+
+    public HistoricalCollectedData(StormTopology rawTopology, int historySize) {
+        this.rawTopology = rawTopology;
+        this.historySize = historySize;
+    }
+
+    private StormTopology rawTopology;
+    private int historySize;
+    public final Map<String, Queue<AggResult>> compHistoryResults = new HashMap<>();
+
+    public void putResult(String comp, AggResult[] exeAggResult) {
+        AggResult CompAggResult = rawTopology.get_spouts().containsKey(comp) ? new SpoutAggResult() : new BoltAggResult();
+        ///First aggregate executorResults to Component results, vertical combine.
+        AggResult.getVerticalCombinedResult(CompAggResult, Arrays.asList(exeAggResult));
+        ///Next, add aggregated component results to history window
+        compHistoryResults.computeIfAbsent(comp, (k) -> new FixedSizeQueue(historySize)).add(CompAggResult);
+    }
+
+    public void clear() {
+        compHistoryResults.clear();
+    }
+
+}
