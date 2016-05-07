1db79135795ba6b58b71dcf6c03f092f01271e94
fuzhengjia <fuzhengjia@gmail.com>
2016-05-07 19:43:28 +0800

    test 3

--- resa-evaluation/src/main/java/resa/evaluation/scheduler/OnceAdjustMaker.java
+++ resa-evaluation/src/main/java/resa/evaluation/scheduler/OnceAdjustMaker.java
@@ -45,15 +45,15 @@ public class OnceAdjustMaker implements DecisionMaker {
 //            ret.put("matcher", 8);
 //            ret.put("aggregater", 1);
 
-//            ret.put("image-input", 1);
-//            ret.put("feat-ext", 10);
-//            ret.put("matcher", 11);
-//            ret.put("vd-aggregator", 1);
-
             ret.put("image-input", 1);
-            ret.put("feat-ext", 8);
-            ret.put("matcher", 8);
+            ret.put("feat-ext", 10);
+            ret.put("matcher", 11);
             ret.put("vd-aggregator", 1);
+
+//            ret.put("image-input", 1);
+//            ret.put("feat-ext", 8);
+//            ret.put("matcher", 8);
+//            ret.put("vd-aggregator", 1);
 
             ///for resaVLD
 //            ret.put("tVLDSpout", 1);
